a423cc4ad556b666b977d21b28c3879930e45097
Komu <komuw05@gmail.com>
2014-04-08 15:25:28 +0300

    suit myself

---
+++ devops/vagrant_bootstrap.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Set up Ansible on a Vagrant Ubuntu Precise box, then run the development playbook.
+
+if [ ! $(which ansible) ]; then
+  sudo apt-get update && \
+	sudo apt-get -y install python-software-properties && \
+	sudo add-apt-repository -y ppa:rquillo/ansible && \
+	sudo apt-get -y install ansible
+fi
+
+if [ ! $(which sshpass) ]; then
+  sudo apt-get -y install sshpass
+fi
+
+PYTHONUNBUFFERED=1 ansible-playbook -i /vagrant/devops/development /vagrant/devops/site.yml
